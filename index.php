8862f4393ca7e4b804e06e0f501ddc1dce4ad80f
VlGlad <kva-2001@yandex.ru>
2022-08-18 16:16:52 +0500

    mysql connection added

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
 $text = "My first php site!!!";
 
 
+try{
+    $db = new PDO('mysql:host=localhost;dbname=phplearn', 'bduser', 'password');
+} catch (Exception $ex) {
+    die(var_dump($ex));
+}
+
+
 require "index.view.php";
 
